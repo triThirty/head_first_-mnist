9f49082c55e34c6e2957fe7455bb911f4b073741
mao <maoshanshi@cecgw.cn>
2021-03-09 17:35:37 +0800

    use Adam and CrossEntropyLoss, improve accuracy to 93%

--- head_first_ml/mnist.py
+++ head_first_ml/mnist.py
@@ -6,6 +6,23 @@ from torchvision.transforms import ToTensor
 import torch.optim as optim
 
 
+
+data_train = datasets.MNIST(root = "./data/",
+                            transform=ToTensor(),
+                            train = True,
+                            download = False)
+
+data_test = datasets.MNIST(root="./data/",
+                        transform = ToTensor(),
+                        train = False)
+
+data_loader_train = torch.utils.data.DataLoader(dataset=data_train,
+                                                batch_size = 1,
+                                                shuffle = True)
+
+data_loader_test = torch.utils.data.DataLoader(dataset=data_test,
+                                                batch_size = 1,
+                                                shuffle = True)
 class NeuralNetwork(nn.Module):
     def __init__(self):
         super(NeuralNetwork, self).__init__()
@@ -16,65 +33,55 @@ class NeuralNetwork(nn.Module):
     def forward(self, x):
         x = self.flatten(x)
         y = torch.matmul(x, self.W) + self.b
+        # Applies the Softmax function to an n-dimensional 
+        # input Tensor rescaling them so that the elements 
+        # of the n-dimensional output Tensor lie in the range 
+        # [0,1] and sum to 1.
         y = nn.Softmax(dim=1)(y)
         return y
 
 
-def main():
-    data_train = datasets.MNIST(root = "./data/",
-                            transform=ToTensor(),
-                            train = True,
-                            download = False)
+model = NeuralNetwork()
+# optimizer = optim.SGD(params=model.parameters(), lr=0.01)
+optimizer = optim.Adam(params=model.parameters(), lr=0.001) # Change grade desend function from SGD to Adam
 
-    data_test = datasets.MNIST(root="./data/",
-                            transform = ToTensor(),
-                            train = False)
+def train(epoch):
+    for batch_idx, (data, target) in enumerate(data_loader_train): 
+        y = model(data)
+        # y_real = torch.zeros(10, dtype=torch.float).scatter_(0, torch.tensor(target), value=1)
+        # loss = torch.mean(-torch.sum(y_real * torch.log(y)))
+        loss = nn.CrossEntropyLoss()(y.float(), target) # Use cross entrypy loss function to improve accuracy
+        if batch_idx % 10000 == 0:
+            print('Train Epoch: {} [{}/{} ({:.0f}%)]\tLoss: {:.6f}'.
+                format(
+                    epoch,
+                    batch_idx * len(data),
+                    len(data_loader_train.dataset),
+                    100. * batch_idx / len(data_loader_train),
+                    loss.item()))
+        optimizer.zero_grad()
+        loss.backward()
+        optimizer.step()
 
-    data_loader_train = torch.utils.data.DataLoader(dataset=data_train,
-                                                    batch_size = 1,
-                                                    shuffle = True)
-
-    data_loader_test = torch.utils.data.DataLoader(dataset=data_test,
-                                                   batch_size = 1,
-                                                   shuffle = True)
-
-
-    model = NeuralNetwork()
-    print(model)
-    optimizer = optim.SGD([model.W, model.b], lr=0.01)
-    # optimizer = optim.Adam(params=model.parameters(), lr=0.001, weight_decay=0)
-    EPOCHS = 10
-    history = {'Test loss':[],'Test Accuracy':[]}
-    for epoch in range(1,EPOCHS + 1):
-        model.train(True)
-        for batch_idx, (data, target) in enumerate(data_loader_train): 
-            y = model(data)
-            y_real = torch.zeros(10, dtype=torch.float).scatter_(0, torch.tensor(target), value=1)
-            loss = torch.mean(-torch.sum(y_real * torch.log(y)))
-            predictions = torch.argmax(y, dim = 1)
-            accuracy = torch.sum(predictions == target)/target.shape[0]
-            # model.zero_grad()
-            optimizer.zero_grad()
-            loss.backward()
-            optimizer.step()
-
-            if batch_idx%10000 == 0:
-                correct,totalloss = 0,0
-                model.train(False)
-                for testData, testTarget in data_loader_test:
-                    test_y = model(testData)
-                    test_y_real = torch.zeros(10, dtype=torch.float).scatter_(0, torch.tensor(testTarget), value=1)
-                    test_loss = torch.mean(-torch.sum(test_y_real * torch.log(test_y)))
-                    predictions = torch.argmax(test_y,dim = 1)
-                    totalloss += test_loss
-                    correct += torch.sum(predictions == testTarget)
-                testAccuracy = correct/(1 * len(data_loader_test))
-                testloss = totalloss/len(data_loader_test)
-                history['Test loss'].append(testloss.item())
-                history['Test Accuracy'].append(testAccuracy.item())
-                print("[%d/%d] loss: %.4f, Acc: %.4f, Test loss: %.4f, Test Acc: %.4f" % 
-                                    (epoch,EPOCHS,loss.item(),accuracy.item(),testloss.item(),testAccuracy.item()))
+def test():
+    test_loss = 0
+    correct = 0
+    for batch_idx, (data, target) in enumerate(data_loader_test): 
+        y = model(data)
+        # y_real = torch.zeros(10, dtype=torch.float).scatter_(0, torch.tensor(target), value=1)
+        # test_loss += torch.mean(-torch.sum(y_real * torch.log(y)))
+        test_loss += nn.CrossEntropyLoss()(y.float(), target)
+        predictions = torch.argmax(y, dim = 1) # output tensor([5])
+        correct += predictions == target # target tensor([1])
+    accuracy = 100. * correct / len(data_loader_test.dataset)
+    test_loss /= len(data_loader_test.dataset)
+    print('\nTest set: Average loss: {:.4f}, Accuracy: ({:.0f}%)\n'.format(
+        test_loss.item(), accuracy.item() ))
+        
                 
 
 if __name__ == '__main__':
-    main()
+    for epoch in range(1, 10):
+        print("test num"+str(epoch))
+        train(epoch)
+        test()
